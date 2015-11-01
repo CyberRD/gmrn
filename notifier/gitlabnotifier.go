6cf2c1d592078637106b9505bbde0210a60088cc
eternnoir <eternnoir@gmail.com>
2015-11-01 19:00:10 +0800

    Support scan muti projects.

--- notifier/gitlabnotifier.go
+++ notifier/gitlabnotifier.go
@@ -1,23 +1,26 @@
 package notifier
 
 import (
-	//log "github.com/Sirupsen/logrus"
+	log "github.com/Sirupsen/logrus"
 	"github.com/eternnoir/gmrn/apis"
 	"time"
 )
 
-func InitGitLabNotifier(url, token string, interval time.Duration) *GitLabNotifier {
+func InitGitLabNotifier(url, token string, projects []string, interval time.Duration) *GitLabNotifier {
 	gitlab := GitLabNotifier{}
 	gitlab.Url = url
 	gitlab.Token = token
+	gitlab.Projects = projects
 	gitlab.Interval = interval
 	gitlab.Api = apis.InitGitlabApi(url, token)
+	log.Infof("Init GitLabNotifier. Url:%s , Toke:%s, %d projects.", url, token, len(projects))
 	return &gitlab
 }
 
 type GitLabNotifier struct {
 	Url      string
 	Token    string
+	Projects []string
 	Interval time.Duration
 	Api      *apis.GitLabApi
 }
